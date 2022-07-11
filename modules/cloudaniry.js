dd9dd528be747ba0d61bb7722766db1c82907bc0
Amit Yadav <87850377+Amit-Yadav21@users.noreply.github.com>
2022-07-11 11:24:03 +0530

    Update cloudaniry.js

--- modules/cloudaniry.js
+++ modules/cloudaniry.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 const cloud = cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME || "amitcloudinary",
-    api_key: process.env.CLOUDINERY_API_KEY || 823599843918524,
-    api_secret: process.env.CLOUDINERY_SECREAT_KEY || "-DY4K2DHuH18sE0zuhwEz8qQhIQ",
+    cloud_name: process.env.CLOUD_NAME || "here",
+    api_key: process.env.CLOUDINERY_API_KEY || here,
+    api_secret: process.env.CLOUDINERY_SECREAT_KEY || "here",
     secure: true
 })
 
